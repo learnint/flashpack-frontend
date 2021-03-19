5a66ee1881e4718eb18c49b5f716320abdcd35d5
Jarod Burchill <jburchill3780@conestogac.on.ca>
2021-03-19 17:55:03 -0400

    Clear queries on logout

--- src/auth/useAuth.tsx
+++ src/auth/useAuth.tsx
@@ -1,16 +1,18 @@
 import React, { createContext, useContext, useState } from "react";
 import { useToast } from "@chakra-ui/react";
+import { useQueryClient } from "react-query";
 import { useHistory } from "react-router-dom";
 import { useLocationState } from "router";
 import { useMutateLogin } from "api";
 
-interface Auth {
+interface AuthContext {
   accessToken: string | undefined;
   login: (email: string, password: string) => Promise<void>;
   logout: () => void;
 }
 
-const AuthContext = createContext<Auth | undefined>(undefined);
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+const AuthContext = createContext<AuthContext | undefined>(undefined);
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
@@ -26,13 +28,14 @@ const useAuthProvider = () => {
   const history = useHistory();
   const { from } = useLocationState();
 
+  const queryClient = useQueryClient();
+  const mutateLogin = useMutateLogin();
+
   const toast = useToast();
 
   const [accessToken, setAccessToken] = useState<string | undefined>(
     localStorage.getItem("accessToken") || undefined
   );
-
-  const mutateLogin = useMutateLogin();
 
   const login = async (email: string, password: string) => {
     try {
@@ -52,6 +55,7 @@ const useAuthProvider = () => {
 
   const logout = () => {
     // history.push("/");
+    queryClient.clear();
     setAccessToken(undefined);
     localStorage.removeItem("accessToken");
   };
@@ -64,6 +68,9 @@ const useAuthProvider = () => {
 };
 
 export const AuthProvider: React.FC = ({ children }) => {
-  const auth = useAuthProvider();
-  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={useAuthProvider()}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
