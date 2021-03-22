07cd1a92668f24c43a5241d744d124095fc610c2
gsacco <gsacco6111@conastogac.on.ca>
2021-03-22 17:55:08 -0400

    validations update

--- src/validations/User.ts
+++ src/validations/User.ts
@@ -1,22 +1,39 @@
 import * as Yup from "yup";
 
-export const firstName = Yup.string().required("Required");
+export const firstName = Yup.string()
+  .min(3, "First name must be a minimum of 3 characters")
+  .max(30, "First name must be a maximum of 30 characters")
+  .matches(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/, "First name cannot contain numbers")
+  .required("Required");
 
-export const lastName = Yup.string().required("Required");
+export const lastName = Yup.string()
+  .min(3, "Last name must be a minimum of 3 characters")
+  .max(30, "Last name must be a maximum of 30 characters")
+  .matches(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/, "Last name cannot contain numbers")
+  .required("Required");
 
 export const email = Yup.string()
   .email("Invalid email address")
+  .max(70, "Email must be a maximum of 70 characters")
   .required("Required");
 
-export const password = Yup.string().required("Required");
+export const password = Yup.string()
+  .min(6, "Password must be a minimum of 6 characters")
+  .max(30, "Password must be a maximum of 30 characters")
+  .required("Required");
 
 export const confirmPassword = Yup.string()
   .oneOf([Yup.ref("password")], "Passwords do not match")
   .required("Required");
 
-export const oldPassword = Yup.string().required("Required");
+export const oldPassword = Yup.string()
+  .min(6, "Old password must be a minimum of 6 characters")
+  .max(30, "Old password must be a maximum of 30 characters")
+  .required("Required");
 
 export const newPassword = Yup.string()
+  .min(6, "New password must be a minimum of 6 characters")
+  .max(30, "New password must be a maximum of 30 characters")
   .notOneOf(
     [Yup.ref("oldPassword")],
     "New password cannot be the same as old password"
