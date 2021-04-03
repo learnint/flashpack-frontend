20951f887835c771d06e08d9254d7cf499290c23
Jarod Burchill <jburchill3780@conestogac.on.ca>
2021-04-03 00:35:13 -0400

    GroupMembers markup and styling

--- src/components/pages/groups/members/GroupMembers.tsx
+++ src/components/pages/groups/members/GroupMembers.tsx
@@ -1,16 +1,48 @@
 import React from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Stack,
+} from "@chakra-ui/react";
 import { Group } from "models";
+import { useColorScheme } from "theme";
 
 interface GroupMembersProps {
   group: Group;
 }
 
 export const GroupMembers: React.FC<GroupMembersProps> = ({ group }) => {
+  const colorScheme = useColorScheme();
+  const grayColorScheme = useColorScheme("gray", true);
+
   return (
-    <div>
-      {group.users.map((user) => (
-        <div key={user.id}>{user.email}</div>
+    <Stack w="full" maxW="container.sm">
+      <Flex justifyContent="space-between">
+        <Heading color={colorScheme}>{group.name} Members</Heading>
+        {group.isAdmin ? <Button>Invite</Button> : null}
+      </Flex>
+      {group.users.map(({ id, firstName, lastName }, index) => (
+        <React.Fragment key={id}>
+          <Flex alignItems="center">
+            <Avatar
+              name={`${firstName} ${lastName}`}
+              bg={colorScheme}
+              color={grayColorScheme}
+            />
+            <Divider orientation="vertical" mx="4" />
+            <Box>
+              {firstName} {lastName}
+            </Box>
+            <Divider orientation="vertical" ml="auto" />
+          </Flex>
+          {group.users.length !== index + 1 ? <Divider /> : null}
+        </React.Fragment>
       ))}
-    </div>
+      {!group.isAdmin ? <Button colorScheme="red">Leave Group</Button> : null}
+    </Stack>
   );
 };
--- src/theme/useColorScheme.ts
+++ src/theme/useColorScheme.ts
@@ -1,7 +1,11 @@
 import { ThemeTypings, useColorModeValue } from "@chakra-ui/react";
 
 export const useColorScheme = (
-  colorScheme: ThemeTypings["colorSchemes"] | (string & {}) = "purple"
+  colorScheme: ThemeTypings["colorSchemes"] | (string & {}) = "purple",
+  inverted = false
 ) => {
-  return useColorModeValue(`${colorScheme}.600`, `${colorScheme}.200`);
+  const lightValue = !inverted ? `${colorScheme}.600` : `${colorScheme}.200`;
+  const darkValue = !inverted ? `${colorScheme}.200` : `${colorScheme}.600`;
+
+  return useColorModeValue(lightValue, darkValue);
 };
