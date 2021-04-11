b4d8780a742a019047c7727ab2ed25ac13caaeb3
Jarod Burchill <jburchill3780@conestogac.on.ca>
2021-04-11 13:38:42 -0400

    Add required routes in Packs and Pack components

--- src/components/pages/packs/Pack.tsx
+++ src/components/pages/packs/Pack.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Heading } from "@chakra-ui/react";
-import { Redirect, useParams } from "react-router-dom";
+import {
+  Redirect,
+  Route,
+  Switch,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
 import { Pack as PackModel, Group } from "models";
 import { useColorScheme } from "theme";
 
@@ -11,6 +17,7 @@ interface PackProps {
 }
 
 export const Pack: React.FC<PackProps> = ({ isAdmin, packs, group }) => {
+  const { path, url } = useRouteMatch();
   const { packId } = useParams<{ packId: string }>();
   const colorScheme = useColorScheme();
 
@@ -19,10 +26,19 @@ export const Pack: React.FC<PackProps> = ({ isAdmin, packs, group }) => {
   return (
     <>
       {isAdmin && pack ? (
-        <Heading color={colorScheme}>
-          {group ? `${group.name} - ` : null}
-          {pack.name}
-        </Heading>
+        <Switch>
+          <Route path={`${path}/cards`}>
+            <Heading color={colorScheme}>
+              {group ? `${group.name} - ` : null}
+              {pack.name}
+            </Heading>
+            {/* <Cards /> */}
+          </Route>
+          <Route path={`${path}/settings`}>{/* <PackSettings /> */}</Route>
+          <Route path={path}>
+            <Redirect to={`${url}/cards`} />
+          </Route>
+        </Switch>
       ) : (
         <Redirect to={group ? `/groups/${group.id}/packs` : `/packs`} />
       )}
--- src/components/pages/packs/Packs.tsx
+++ src/components/pages/packs/Packs.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Button, Flex, Heading, Skeleton, Stack } from "@chakra-ui/react";
-import { Switch, Route, useRouteMatch } from "react-router-dom";
+import { Switch, Route, useRouteMatch, useHistory } from "react-router-dom";
+import { usePack } from "context";
 import { Group } from "models";
 import { useColorScheme } from "theme";
 import { BlockLink } from "components/common";
 import { Pack } from "./Pack";
-import { usePack } from "context";
 
 interface PacksProps {
   group?: Group;
 }
 
 export const Packs: React.FC<PacksProps> = ({ children, group }) => {
+  const history = useHistory();
   const { path, url } = useRouteMatch();
   const colorScheme = useColorScheme();
 
@@ -22,6 +23,7 @@ export const Packs: React.FC<PacksProps> = ({ children, group }) => {
   if (!isPacksLoading && !isPacksError && packs) {
     return (
       <Switch>
+        <Route path={`${path}/create`}>{/* <CreatePack /> */}</Route>
         <Route path={`${path}/:packId`}>
           <Pack isAdmin={isAdmin} packs={packs} group={group} />
         </Route>
@@ -31,14 +33,24 @@ export const Packs: React.FC<PacksProps> = ({ children, group }) => {
               <Heading color={colorScheme}>
                 {group ? `${group.name} - ` : "My"} Packs
               </Heading>
-              {group ? children : <Button>Create Pack</Button>}
+              {group ? (
+                children
+              ) : (
+                <Button onClick={() => history.push(`${url}/create`)}>
+                  Create Pack
+                </Button>
+              )}
             </Flex>
             {packs.map(({ id, name, description, cardCount }) => (
               <BlockLink
                 to={isAdmin ? `${url}/${id}` : `/quiz/${id}`}
                 name={name}
                 description={description}
-                onEditClick={isAdmin ? () => {} : undefined}
+                onEditClick={
+                  isAdmin
+                    ? () => history.push(`${url}/${id}/settings`)
+                    : undefined
+                }
                 counts={[{ key: "Cards", value: cardCount }]}
                 key={id}
               />
