1367ea72d633a3081c11f3b4735222466b7448d5
Jarod Burchill <jburchill3780@conestogac.on.ca>
2021-04-13 20:31:02 -0400

    Moved <CreateCard> component to correct path

--- src/components/pages/packs/cards/Cards.tsx
+++ src/components/pages/packs/cards/Cards.tsx
@@ -1,26 +1,41 @@
 import React from "react";
 import { Button, Flex, Heading, Stack } from "@chakra-ui/react";
+import { Route, Switch, useHistory, useRouteMatch } from "react-router-dom";
 import { useColorScheme } from "theme";
 import { Pack } from "models";
 import { CreateCard } from "./create";
+import { Card } from "./Card";
 
 interface CardsProps {
   pack: Pack;
 }
 
 export const Cards: React.FC<CardsProps> = ({ children, pack }) => {
+  const history = useHistory();
+  const { path, url } = useRouteMatch();
   const colorScheme = useColorScheme();
 
   return (
-    <Stack w="full" maxW="container.lg">
-      <Flex justifyContent="space-between">
-        <Heading color={colorScheme}>{pack.name} - Cards</Heading>
-        <Flex justifyContent="flex-end" wrap="wrap">
-          {children}
-          <Button ml="2">Create Card</Button>
-        </Flex>
-      </Flex>
-      <CreateCard pack={pack} groupId="" />
-    </Stack>
+    <Switch>
+      <Route path={`${path}/create`}>
+        <CreateCard pack={pack} groupId="" />
+      </Route>
+      <Route path={`${path}/:cardId`}>
+        <Card />
+      </Route>
+      <Route path={path}>
+        <Stack w="full" maxW="container.lg">
+          <Flex justifyContent="space-between">
+            <Heading color={colorScheme}>{pack.name} - Cards</Heading>
+            <Flex justifyContent="flex-end" wrap="wrap">
+              {children}
+              <Button ml="2" onClick={() => history.push(`${url}/create`)}>
+                Create Card
+              </Button>
+            </Flex>
+          </Flex>
+        </Stack>
+      </Route>
+    </Switch>
   );
 };
